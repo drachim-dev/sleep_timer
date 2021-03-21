c191e131d8d5fb445fa460124d68a97f6128fbdc
Tim Wiechmann <Tim.Wiechmann@web.de>
2021-03-21 17:14:00 +0100

    Add admob testIds as defaultValues.

--- lib/common/ad_manager.dart
+++ lib/common/ad_manager.dart
@@ -1,11 +1,10 @@
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+
 class AdManager {
-  static const String appId = String.fromEnvironment('ADMOB_APP_ID');
-
-  static const String bannerAdUnitId =
-      String.fromEnvironment('ADMOB_BANNER_AD_ID');
-
-  static const String nativeAdUnitId =
-      String.fromEnvironment('ADMOB_NATIVE_AD_ID');
+  static String nativeAdUnitId = String.fromEnvironment(
+    'ADMOB_NATIVE_AD_ID',
+    defaultValue: NativeAd.testAdUnitId,
+  );
 
   static const testDevice1 = String.fromEnvironment('ADMOB_TEST_DEVICE_ID_1');
   static const testDevice2 = String.fromEnvironment('ADMOB_TEST_DEVICE_ID_2');
