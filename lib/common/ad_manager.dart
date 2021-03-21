c4458ef3efa47b0348488c3679ad567ebf8b3ddc
Tim Wiechmann <Tim.Wiechmann@web.de>
2021-03-21 20:15:05 +0100

    Need to use const for dart define variables.

--- lib/common/ad_manager.dart
+++ lib/common/ad_manager.dart
@@ -1,10 +1,8 @@
-import 'package:google_mobile_ads/google_mobile_ads.dart';
-
 class AdManager {
-  static String nativeAdUnitId = String.fromEnvironment(
-    'ADMOB_NATIVE_AD_ID',
-    defaultValue: NativeAd.testAdUnitId,
-  );
+  /// Must be 'const' when using String.fromEnvironment
+  static const String nativeAdUnitId = String.fromEnvironment(
+      'ADMOB_NATIVE_AD_ID',
+      defaultValue: 'ca-app-pub-3940256099942544/2247696110');
 
   static const testDevice1 = String.fromEnvironment('ADMOB_TEST_DEVICE_ID_1');
   static const testDevice2 = String.fromEnvironment('ADMOB_TEST_DEVICE_ID_2');
